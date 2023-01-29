e10775a0b8922e2e2f664b1b2d4346c641afbbf2
Oluwatobiloba Ezekiel <mroluwatobilobaezekiel@gmail.com>
2023-01-29 11:43:53 +0100

    corrected arrangement issues with the css, and fix the bug in js

--- infoCard/index.js
+++ infoCard/index.js
@@ -2,5 +2,5 @@ const button = document.querySelector(".card-btn");
 const infoCard = document.querySelector(".blog-card");
 
 button.addEventListener("click",()=>{
-    infoCard.classList.toggle("change") ? (button, firstElementChild.textContent = "Less"):(button, firstElementChild.textContent = "More");
+    infoCard.classList.toggle("change")
 });
